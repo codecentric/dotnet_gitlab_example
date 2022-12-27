c5052f787a8158f955beaefbf362764ddb65e516
ChristianSauer <christian.sauer@codecentric.de>
2022-12-27 15:10:31 +0100

    nested folder removed

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Test");
-            Console.WriteLine("Hello, World!");
-            
+Console.WriteLine("Hello, World!");
